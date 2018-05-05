08ce839680e27b7658bad7b890a5da162107cbd6
lopezfjose <jflopezfernandez@gmail.com>
2018-05-04 21:47:15 -0400

    Done for today

--- Boost-Lib/main.cpp
+++ Boost-Lib/main.cpp
@@ -1,17 +1,68 @@
 
+#include <Windows.h>
+#include <experimental/filesystem>
+
+#include <assert.h>
+#include <math.h>
+#include <time.h>
+
+#include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
+#include <numeric>
+#include <sstream>
 #include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
-#include <boost/chrono.hpp>
-#include <boost/optional.hpp>
-#include <boost/program_options.hpp>
+#include <boost/proto/proto.hpp>
+#include <boost/typeof/std/ostream.hpp>
 
+template <int I>
+struct placeholder
+{ };
 
-int main()
+boost::proto::terminal<placeholder<0>>::type const _1 = { { } };
+boost::proto::terminal<placeholder<1>>::type const _2 = { { } };
+
+boost::proto::terminal<std::ostream&>::type cout_ = { std::cout };
+
+template <typename Expr>
+void evaluate(const Expr& expr) {
+	boost::proto::default_context context;
+	boost::proto::eval(expr, context);
+}
+
+struct calculator_context
+	: boost::proto::callable_context<calculator_context const> {
+	// Values to replace the placeholders
+	std::vector<double> args;
+
+	// Define the result type of the calculator.
+	// (This makes the calculator_context "callable")
+	using result_type = double;
+
+	// Handle the placeholders
+	template <int I>
+	double operator()(boost::proto::tag::terminal, placeholder<I>) const {
+		return this->args[I];
+	}
+};
+
+int main(int argc, char *argv[]) 
 {
-	boost::optional<std::string> opt = boost::make_optional<std::string>("yes");
+	calculator_context ctx;
+	ctx.args.emplace_back(45); // The value of _1 is 45
+	ctx.args.emplace_back(50); // The value of _2 is 50
 
-	std::cout << "Did it work? " << opt.get() << "\n";
+	// Create an arithmetic expression and immediately evaluate it
+	const auto d = boost::proto::eval((_2 - _1) / _2 * 100, ctx);
+
+	// This prints "10"
+	std::cout << "d: " << d << "\n";
+
+	return EXIT_SUCCESS;
 }
